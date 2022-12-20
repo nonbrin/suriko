ce3a13ffe6c5b1761970b10cfda795aca336710f
tae <>
2022-12-21 02:49:30 +0900

    js修正

--- js/script.js
+++ js/script.js
@@ -2,31 +2,49 @@
   // alert('OK!');
 
 
-$(function () {
-  $(".text-animation").each(function () {
-    // 1文字ずつ<span>で囲む
-    $(this)
-      .children()
-      .addBack()
-      .contents()
-      .each(function () {
-        if (this.nodeType == 3) {
-          $(this).replaceWith(
-            $(this).text().replace(/(\S)/g, "<span>$1</span>")
-          );
-        }
-      });
-    // スクロールして要素が画面内に入ったら文字を表示
-    $(this).on("inview", function () {
-      // 1文字ずつ順番に表示(不透明にする)
-      $(this).css({ opacity: 1 });
-      for (var i = 0; i <= $(this).children("span").length; i++) {
-        $(this)
-          .children("span")
-          .eq(i)
-          .delay(200 * i) // 文字間の時間
-          .animate({ opacity: 1 }, 1000); // 全部表示されるまでの時間
+  var id = ['anemasion']; //指定するidを全て配列で渡す
+  var tx = [];
+  var txCount = [];
+  var txSp = 100; // テキストの表示速度
+  var dly = 1000; // 次の文章までの待ち時間
+  var count = 0;
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+  
+  window.onload = function(){
+    kamikakushi();
+    countSet();
+    itimozi()
+  }
+  
+  function countSet(){ // 文字数カウントの初期設定
+    for(n=0;n<id.length;n++){
+      txCount[n] = 0;
+    }
+  }
+  
+  
+  function kamikakushi(){ // 要素を取得して非表示（opacity:0;）にする
+    for(i=0;i<id.length;i++){
+      id[i] = document.getElementById(id[i]);
+      tx[i] = id[i].firstChild.nodeValue; // 初期の文字列
+      id[i].innerHTML = '';
+    }
+  }
+  
+  function itimozi(){ //一文字ずつ表示
+    id[count].innerHTML = tx[count].substr( 0, ++txCount[count] )+" "; // テキストの指定した数の間の要素を表示
+    if(tx[count].length != txCount[count]){ // Count が初期の文字列の文字数と同じになるまでループ
+      setTimeout("itimozi()",txSp); // 次の文字へ進む
+    }else{
+    id[count].innerHTML = tx[count].substr( 0, ++txCount[count] ); // テキストの指定した数の間の要素を表示
+      count++; // 次の段落に進む為のカウントアップ
+      if(count != id.length){ // id数が最後なら終了
+      setTimeout("itimozi()",dly); // 次の段落へ進む
       }
-    });
-  });
-});
+    }
+  }
