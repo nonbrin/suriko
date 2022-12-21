c850c61fc46893a1d3f6bf0bdd1525c13061fc4c
nonbrin <xx@gmail.com>
2022-12-21 19:24:38 +0900

    スクロールに合わせたfeedupアニメーション用コード追加（コード反映漏れ分）

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-
-  // alert('OK!');
-
-
+//タイプライターアニメーション
   var id = ['anemasion']; //指定するidを全て配列で渡す
   var tx = [];
   var txCount = [];
@@ -48,3 +45,19 @@ $(window).scroll(function () {
       }
     }
   }
+
+
+// 参考コード（https://suzunon.com/web-develop/javascript-scroll-animation/）
+
+let targets = document.querySelectorAll('.js-scroll-fall'); //ターゲット要素
+//スクロールイベント
+window.addEventListener('scroll', function () {
+  var scroll = window.scrollY; //スクロール量を取得
+  var windowHeight = window.innerHeight; //画面の高さを取得
+  for (let target of targets) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+    var targetPos = target.getBoundingClientRect().top + scroll + 300; //ターゲット要素の位置を取得
+    if (scroll > targetPos - windowHeight) { //スクロール量 > ターゲット要素の位置
+      target.classList.add('is-animated'); //is-animatedクラスを加える
+    }
+  }
+});
